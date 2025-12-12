f88dd3e37d7b976e865f98552083cc0022663f23
Alejandra <malejart@gmail.com>
2025-12-12 15:49:39 +0100

    fix(App): Remove unused TimelinesList route from application

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Home from './pages/Home.tsx';
 import LifeTimeline from './pages/LifeTimeline.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 import UserProfilePage from './pages/UserProfilePage.tsx';
-import TimelinesList from './pages/TimelinesList.tsx';
 
 // import Link from '@mui/material/Link'
 
@@ -31,7 +30,6 @@ function App() {
           {/* <div className='central-section bb-black h-full w-[50%] m-auto'> */}
           <Routes>
             <Route path="/timelines" element={<OnlyPrivate><TimelinesPage/></OnlyPrivate>} />
-            <Route path="/timelinesList" element={<OnlyPrivate><TimelinesList/></OnlyPrivate>} />
             <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
             <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
             <Route path="/lifetimeline" element={<OnlyPrivate> <LifeTimeline /> </OnlyPrivate>} />
